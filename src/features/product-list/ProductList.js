479779c6d7ba385ad10c5e0476f8ff540223466f
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2023-07-18 23:26:19 +0530

    Navigation Bar Completed

--- src/features/product-list/ProductList.js
+++ src/features/product-list/ProductList.js
@@ -246,7 +246,7 @@ export function ProductList() {
             <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
               <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
                 <h1 className="text-4xl font-bold tracking-tight text-gray-900">
-                  New Arrivals
+                  All Products
                 </h1>
 
                 <div className="flex items-center">
@@ -399,11 +399,11 @@ export function ProductList() {
                   <div className="lg:col-span-3">
                     {/*This is Product List*/}
                     <div className="bg-white">
-                      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+                      <div className="mx-auto max-w-2xl px-4 py-0 sm:px-0 sm:py-0 lg:max-w-7xl lg:px-8">
+                       {/* <h2 className="text-2xl font-bold tracking-tight text-gray-900">
                           Products
                         </h2>
-
+                      */}
                         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                           {products.map((product) => (
                             <div key={product.id} className="group relative">
