0afc9bebf0ceab16bd7f6b39a62dae39bc54e7c8
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2023-07-19 22:07:07 +0530

    Authentication UI Created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './App.css';
 import ProductList  from './features/product-list/ProductList';
 import Home from './pages/Home';
+import LoginPage  from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+    {/*
+      <Home/>
+      <LoginPage/>
+    */}
+    <SignupPage/>
     </div>
   );
 }
