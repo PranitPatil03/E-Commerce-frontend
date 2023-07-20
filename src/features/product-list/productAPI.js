55a08f53c42b7508dedeee817a5c30a19e54c059
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2023-07-20 16:06:30 +0530

    Pagination Created

--- src/features/product-list/productAPI.js
+++ src/features/product-list/productAPI.js
@@ -7,16 +7,28 @@ export function fetchAllProducts() {
   );
 }
 
-export function fetchProductsByFilters(filter) {
+export function fetchProductsByFilters(filter,sort,pagination) {
   let queryString = '';
   for(let key in filter){
-    queryString += `${key}=${filter[key]}&`
+    const categoryValues = filter[key];
+    if(categoryValues.length){
+      const lastCategoryValue = categoryValues[categoryValues.length-1]
+      queryString += `${key}=${lastCategoryValue}&`
+    }
+  }
+  for(let key in sort){
+    queryString += `${key}=${sort[key]}&`
+  }
+  console.log(pagination)
+  for(let key in pagination){
+    queryString += `${key}=${pagination[key]}&`
   }
 
   return new Promise(async (resolve) =>{
     const response = await fetch('http://localhost:8080/products?'+queryString) 
     const data = await response.json()
-    resolve({data})
+    const totalItems = await response.headers.get('X-Total-Count')
+    resolve({data:{products:data,totalItems:+totalItems}})
   }
   );
 }
