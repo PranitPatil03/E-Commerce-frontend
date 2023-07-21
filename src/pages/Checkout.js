37c850893900caa67de0d601dd95402ab5d8e6a5
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2023-07-21 17:53:08 +0530

    Order feature created

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -11,53 +11,7 @@ import {
   updateUserAsync,
 } from "../features/auth/authSlice";
 import { useState } from "react";
-
-const products = [
-  {
-    id: 1,
-    name: "Throwback Hip Bag",
-    href: "#",
-    color: "Salmon",
-    price: "$90.00",
-    quantity: 1,
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
-    imageAlt:
-      "Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.",
-  },
-  {
-    id: 2,
-    name: "Medium Stuff Satchel",
-    href: "#",
-    color: "Blue",
-    price: "$32.00",
-    quantity: 1,
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
-    imageAlt:
-      "Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.",
-  },
-  // More products...
-];
-
-const addresses = [
-  {
-    name: "John wick",
-    street: "11th Main",
-    city: "Delhi",
-    pinCode: 110001,
-    state: "Delhi",
-    phone: 12312321331,
-  },
-  {
-    name: "John Doe",
-    street: "15th Main",
-    city: "Bangalore",
-    pinCode: 560034,
-    state: "Karnataka",
-    phone: 123123123,
-  },
-];
+import { createOrderAsync } from "../features/order/orderSlice";
 
 function Checkout() {
   const dispatch = useDispatch();
@@ -107,7 +61,7 @@ function Checkout() {
       paymentMethod,
       selectedAddress,
     };
-    // dispatch(createOrderAsync(order))
+    dispatch(createOrderAsync(order))
   };
 
   return (
