eca3e8c46d3b9ac1c6cd519ee94a7cea98ff605c
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2023-07-31 00:01:18 +0530

    Refactor

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../features/navbar/Navbar";
 
 function Start() {
-  return (
+  return (  
     <>
       <Navbar />
       <div className="relative overflow-hidden bg-white">
